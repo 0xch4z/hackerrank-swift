8437e9770ad316bd817416c3b127078a9e663c01
Charles Kenney <Charlesc.kenney@gmail.com>
2017-05-29 02:10:20 -0400

    updated 'Equal Stacks' problem

--- Data Structures/Stacks/Equal Stacks/EqualStacks.swift
+++ Data Structures/Stacks/Equal Stacks/EqualStacks.swift
@@ -1,95 +1,21 @@
-import Foundation;
+iimport Foundation;
 
-// Get input
+// Get elements; get stack height
 _ = readLine()!.components(separatedBy: " ").map{ Int($0)! } // irrelavent
-let stack1Elements = readLine()!.components(separatedBy: " ").map{ Int($0)! }
-let stack2Elements = readLine()!.components(separatedBy: " ").map{ Int($0)! }
-let stack3Elements = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+var stack1 = readLine()!.components(separatedBy: " ").map{ Int($0)! }; var stack1Height = stack1.reduce(0, +)
+var stack2 = readLine()!.components(separatedBy: " ").map{ Int($0)! }; var stack2Height = stack2.reduce(0, +)
+var stack3 = readLine()!.components(separatedBy: " ").map{ Int($0)! }; var stack3Height = stack3.reduce(0, +)
 
-// MARK: - *Special* Stack Model
-struct Stack {
-    public var items = [Int]()
-    public var history = [Int]()
-    public var isEmpty: Bool {
-        return (items.count == 0)
-    }
-    public var height: Int {
-        var currentHeight = 0
-        for element in items {
-            currentHeight += element
-        }
-        return currentHeight
-    }
-    // Appends element
-    mutating func push(_ element: Int) {
-        items.append(element)
-        if !history.contains(height) {
-            history.append(height)
-        }
-    }
-    // Returns head and discards
-    mutating func pop() -> Int? {
-        if !isEmpty {
-            return items.removeLast()
-        } else {
-            return nil
-        }
-    }
-    // Returns head without discarding
-   func peek() -> Int? {
-        if !isEmpty {
-            return items.last
-        } else {
-            return nil
-        }
+// Remove elements until stacks are equal in height
+while stack1Height != stack2Height || stack1Height != stack3Height || stack2Height != stack3Height {
+   if (stack1Height >= stack2Height) && (stack1Height >= stack3Height) {
+        stack1Height -= stack1.removeFirst()
+    } else if (stack2Height >= stack1Height) && (stack2Height >= stack3Height) {
+        stack2Height -= stack2.removeFirst()
+    } else if (stack3Height >= stack1Height) && (stack3Height >= stack2Height) {
+        stack3Height -= stack3.removeFirst()
     }
 }
 
-
-// MARK: - Binary Search Function
-func binarySearch<T: Comparable>(_ arr: [T], key: T, range: Range<Int>) -> Bool {
-    if range.lowerBound >= range.upperBound {
-        return false
-    } else {
-        let midIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
-        if arr[midIndex] > key {
-            return binarySearch(arr, key: key, range: range.lowerBound ..< midIndex)
-        } else if arr[midIndex] < key {
-            return binarySearch(arr, key: key, range: midIndex + 1 ..< range.upperBound)
-        } else {
-            return true
-        }
-    }
-}
-
-// MARK: - Pattern Specifc Stack Initializer
-func initializeStackWithDataSet(elements: [Int]) -> Stack {
-    var stack = Stack()
-    // push all predefined elements to stack instance
-    for element in elements.reversed() {
-        stack.push(element)
-    }
-    // return stack
-    return stack
-}
-
-// MARK: - Stack Input Processing
-var stack1 = initializeStackWithDataSet(elements: stack1Elements)
-var stack2 = initializeStackWithDataSet(elements: stack2Elements)
-var stack3 = initializeStackWithDataSet(elements: stack3Elements)
-
-func commonHeights() -> Int {
-    var result = 0
-    for height in stack1.history.reversed() {
-        if binarySearch(stack2.history, key: height, range: 0..<stack2.history.count) && binarySearch(stack3.history, key: height, range: 0..<stack3.history.count) {
-            result = height
-            return result
-        }
-    }
-    return result
-}
-
-// Returns max common stack height
-let maxCommonHeight: Int = commonHeights()
-
-print(maxCommonHeight)
+// Return equal height
+print(stack1Height)
